700bfb2561f9c14f75e72e4d20d1c1de4e23679d
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-07-12 15:44:30 -0700

    style button and counter

--- app/screens/ButtonScreen.js
+++ app/screens/ButtonScreen.js
@@ -29,13 +29,14 @@ export default class ButtonScreen extends Component {
         <Text>{this.props.screenProps.connectedDevice.name}</Text>
         <Text>Last click: {this.props.screenProps.lastClick}</Text>
         <NavBar/>
-          <TouchableOpacity style={styles.circle} onPress={this.onPress}>
-            <Text style={styles.alignMe}> Push </Text>
+          <TouchableOpacity style={styles.button} onPress={this.onPress}>
+            {/* <Text style={styles.buttonText}> Push </Text> */}
+            <Text style={[styles.countText]}>
+              { this.state.count !== 0 ? this.state.count : 'Push'}
+            </Text>
           </TouchableOpacity>
           <View style={[styles.countContainer]}>
-            <Text style={[styles.countText]}>
-              { this.state.count !== 0 ? this.state.count: null}
-            </Text>
+
           </View>
       </View>
     )
@@ -49,42 +50,27 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     alignItems: 'center'
   },
-  alignMe: {
-    // flex: 1,
-    // alignItems: 'center',
-    // textAlign: 'center',
-    // marginTop: 0,
-    // fontStyle: 'italic'
-    fontSize: 33
-  },
-  // button: { // not using this now
-  //   alignItems: 'center',
-  //   backgroundColor: '#ff00ff',
-  //   padding: 10,
-  //   margin: 50
-  // },
   countContainer: {
     alignItems: 'center',
     padding: 3
   },
   countText: {
     color: 'black',
-    backgroundColor: 'cyan'
+    fontSize: 33,
+    color: '#414141'
   },
-  circle: { // replaced button with this style
-    // flexDirection: 'column',
-    // flex: 1,
+  button: { // replaced button with this style
     justifyContent: 'center',
     alignItems: 'center',
     width: 150,
     height: 150,
     borderRadius: 150/2,
-    backgroundColor: 'turquoise',
+    backgroundColor: '#b8d6ce',
     alignItems: 'center',
     padding: 10,
     margin: 120,
-    borderWidth:5,
-    borderColor: 'darkblue',
+    // borderWidth:5,
+    // borderColor: 'darkblue',
     alignItems: 'center'
   }
 });
