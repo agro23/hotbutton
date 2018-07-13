dc25a43ce1f5ddefe7852272c37b8407e924b6cf
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-07-12 18:10:11 -0700

    add DeviceCard component for displayed connected device info/status

---
+++ app/components/DeviceCard.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+
+class DeviceCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+    return(
+      <View style={styles.navContainer}>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  cardContainer: {
+
+  }
+})
